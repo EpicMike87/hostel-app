e06711361b0253c6c5892a060caacbe740102edf
EpicMike87 <mander234@caledonian.ac.uk>
2023-12-16 00:30:27 +0000

    Adding contact detail popup

--- src/components/HostelDetails.js
+++ src/components/HostelDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Star from "./Star";
 
 const HostelDetails = ({ hostel }) => {
@@ -12,29 +12,65 @@ const HostelDetails = ({ hostel }) => {
   };
 
   const averageRating = calculateAverageRating();
+  const [showPhonePopup, setShowPhonePopup] = useState(false);
+  const [showEmailPopup, setShowEmailPopup] = useState(false);
+
+  const togglePhonePopup = () => {
+    setShowPhonePopup(!showPhonePopup);
+    setShowEmailPopup(false); // Close the email popup when opening phone popup
+  };
+
+  const toggleEmailPopup = () => {
+    setShowEmailPopup(!showEmailPopup);
+    setShowPhonePopup(false); // Close the phone popup when opening email popup
+  };
 
   return (
     <div style={{ width: "30%", padding: "20px" }}>
       {hostel && (
         <>
-          <h2>{hostel.name}</h2> <p>{hostel.postcode}</p>
-          <p>{hostel.description}</p>
+          <h2>{hostel.name}</h2>
+          <p>{hostel.postcode}</p>
+          <p>{hostel.description}</p> 
           <div>
             {Array.from({ length: 5 }).map((_, index) => (
-              <Star
-                key={index}
-                selected={index < averageRating}
-              />
+              <Star key={index} selected={index < averageRating} />
             ))}
             <div className="contact-section">
-              <button className="contact-button">
-                <img src="/icons/envelope-solid.svg" alt="Email Icon" style={{ width: "25px", margin: "5px" }} />
+              <button
+                className="contact-button"
+                onClick={togglePhonePopup}
+              >
+                <img
+                  src="/icons/phone-solid.svg"
+                  alt="Phone Icon"
+                  style={{ width: "25px", margin: "5px" }}
+                />
               </button>
-              <button className="contact-button">
-                <img src="/icons/phone-solid.svg" alt="Phone Icon" style={{ width: "25px", margin: "5px" }} />
+              <button
+                className="contact-button"
+                onClick={toggleEmailPopup}
+              >
+                <img
+                  src="/icons/envelope-solid.svg"
+                  alt="Email Icon"
+                  style={{ width: "25px", margin: "5px" }}
+                />
               </button>
+            </div>
           </div>
-          </div>
+
+          {showPhonePopup && (
+            <div className="contact-popup">
+              <p>Phone: {hostel.phone}</p>
+            </div>
+          )}
+
+          {showEmailPopup && (
+            <div className="contact-popup">
+              <p>Email: {hostel.email}</p>
+            </div>
+          )}
         </>
       )}
     </div>
